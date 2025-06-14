1296d36034d1c394818d31e5b8533adced9be0b2
Yash <maddysai9848@gmail.com>
2025-06-14 13:59:17 +0530

    added theme gruvbox

--- lua/plugins/LuaLine.lua
+++ lua/plugins/LuaLine.lua
@@ -3,12 +3,14 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
      config = function()
     -- load the tokyonight colorscheme first
-      vim.cmd.colorscheme("tokyonight")
+      -- vim.cmd.colorscheme("tokyonight")
+      vim.cmd.colorscheme("gruvbox")
 
     -- then set lualine to use that theme
       require('lualine').setup {
       options = {
-        theme = 'tokyonight',
+        -- theme = 'tokyonight',
+        theme = 'gruvbox',
         icons_enabled = true,
         section_separators = { left = '', right = '' },
         component_separators = '|',
